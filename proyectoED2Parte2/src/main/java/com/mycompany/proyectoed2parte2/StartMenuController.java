bdd35e9299f0659fa139145214a0839b4212178f
RobertoPatino1 <rapatino@espol.edu.ec>
2022-08-15 22:55:34 -0500

    Agregada funcionalidad de cargar archivos de preguntas y respuestas en conjunto

--- proyectoED2Parte2/src/main/java/com/mycompany/proyectoed2parte2/StartMenuController.java
+++ proyectoED2Parte2/src/main/java/com/mycompany/proyectoed2parte2/StartMenuController.java
@@ -5,15 +5,25 @@
  */
 package com.mycompany.proyectoed2parte2;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
 
 
 public class StartMenuController implements Initializable {
-
+public static  int id = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -22,13 +32,49 @@ public class StartMenuController implements Initializable {
     
     @FXML
     private void jugar(ActionEvent event) {
+        //Saltamos de ventana???
     }
 
     @FXML
-    private void cargarPreguntas(ActionEvent event) {
-        /*
-        Metodo que abre un fileChooser para agregar los archivos de texto con las preguntas
-        */
+    private void cargarPreguntas(ActionEvent event) throws IOException {
+
+        try{
+        FileChooser fileChooser = new FileChooser(); //Este nos permite abrir el explorador de archivo
+        fileChooser.setTitle("Buscar foto");
+       
+        
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT", "*.txt"));
+        
+
+        List<File> txtFile = fileChooser.showOpenMultipleDialog(null);
+        
+        if(txtFile!=null){
+            for(File f: txtFile){
+               File txt = new File("file:"+f.getAbsolutePath()); 
+                //Se copia la imagen
+                Path from = Paths.get(f.toURI());
+                Path to = Paths.get("archivos/"+id+"_"+f.getName());
+
+                Files.copy(from, to);
+                
+                
+            }
+            id++;
+            
+
+            
+        }
+        
+
+    
+       }catch(FileAlreadyExistsException e){
+            Alert alerta = new Alert(Alert.AlertType.ERROR,"El archivo que se ha seleccionado ya se encuentra almacenado, por favor seleccione otro"); //FIXME
+            alerta.setTitle("Error");
+            alerta.setHeaderText("Ha ocurrido un error:");
+            System.out.println(e.getClass());
+            alerta.show();  
+       }
     }
 
     @FXML
